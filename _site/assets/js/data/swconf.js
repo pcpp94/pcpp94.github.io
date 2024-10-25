ac06252d5202d71d5761afdd3756fad041a18c87
Pablo <pcparedesp@hotmail.com>
2024-10-25 18:37:31 +0400

    changing_link_for_span

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1729865684',resources: [
+    cacheName: 'chirpy-1729867008',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
